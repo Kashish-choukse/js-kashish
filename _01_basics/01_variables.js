7aee8310a2abb21b65d163e22450705652776621
Kashish-choukse <kashishchoukse10@gmail.com>
2025-11-30 02:12:54 +0000

    Variables

---
+++ _01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "kashish@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+accountEmail = "hajdjf.com"
+accountPassword = "2210011881"
+accountCity = "Pune"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
